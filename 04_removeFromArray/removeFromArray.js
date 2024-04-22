c3751323031eb60ba514b711fba532745ef40f5c
Wako Anindo <wakoanindo@gmail.com>
2024-04-22 23:42:05 +0100

    Work through exercises and pass each test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    valuesToRemove.forEach(value => {
+        while (arr.includes(value)) {
+            const index = arr.indexOf(value);
+            arr.splice(index, 1);
+        }
+    });
 
+    return arr;
 };
 
 // Do not edit below this line
