02d9086abfa6d9c8c43055cc15e4d475489fea5a
Wako Anindo <wakoanindo@gmail.com>
2024-04-24 15:43:56 +0100

    Write a solution that returns true or false if a year is or is not a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+// A year is considered a leap year if it meets one of the following conditions:
 
+// It is evenly divisible by 4 but not evenly divisible by 100.
+// It is evenly divisible by 400.
+const leapYears = function(year) {
+ if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return true
+ } else {
+    return false
+ }
 };
 
 // Do not edit below this line
