44d376438fa48c9a42d5dddd851b1da13935a2ab
Wako Anindo <wakoanindo@gmail.com>
2024-04-24 15:20:41 +0100

    Write logic that returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,12 @@
 const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number') return 'ERROR'
     let sum = 0;
 
     const max = Math.max(start, end);
     const min = Math.min(start, end);
 
-    for (i = max; i >= min; i--) {
-        if (i < 0) return 'ERROR';
-        // if ()
+    for (let i = max; i >= min; i--) {
+        if (i < 0) return 'ERROR'
         sum += i;
     }
 
