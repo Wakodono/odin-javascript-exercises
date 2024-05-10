c247c877f1fe62b14a0438e477b59da26be75d3f
Wako Anindo <wakoanindo@gmail.com>
2024-05-10 14:13:35 +0100

    Complete a few more JS fundamentals exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr.reduce((sum, currentItem) => sum + currentItem, 0);
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  return arr.reduce((acc, currentItem) => acc * currentItem);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let result = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+// const power = (base, exponent) => Math.pow(base, exponent);
+
+const factorial = function (n) {
+  let result = 1;
+  for (let i = n; i >= 1; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
