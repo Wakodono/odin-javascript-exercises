8a33bbfafcebe9c0ad6b24a2cd9fb1c29d080015
Wako Anindo <wakoanindo@gmail.com>
2024-05-13 15:47:46 +0100

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n)
 
+    if (typeof(n) !== "number" || n < 0 || isNaN(n)) {
+        return "OOPS";
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1 || n === 2) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
